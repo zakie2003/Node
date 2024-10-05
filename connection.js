3c514aec0c6e7a9eb07a79674759b82c72801cd7
zakie khan <121384625+zakie2003@users.noreply.github.com>
2024-10-05 10:14:23 +0530

    connection added

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 const sql=require("mysql2");
-const conn=sql.createConnection({user:'root',host:'localhost',password:'2003',database:'shopping'})
+const conn=sql.createConnection({user:'sql12735445',host:'sql12.freesqldatabase.com',password:'uKRC1ax65n',database:'sql12735445'})
 
 module.exports=conn;
